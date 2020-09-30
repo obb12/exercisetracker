5c818c2c41c6445ead749c308a33c1a586393d2f
Omar Benchbana <omar.bb@hotmail.fi>
2020-09-30 15:35:54 +0300

    hello

--- config/database.php
+++ config/database.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Str;
+if(getenv('JAWSDB_URL')) {
+  $url = parse_url(getenv('JAWSDB_URL'));
 
+  putenv('DB_HOST='.$url['host']);
+  putenv('DB_PORT='.$url['port']);
+  putenv('DB_USERNAME='.$url['user']);
+  putenv('DB_DATABASE='.substr($url["path"], 1));
+  putenv('DB_PASSWORD='.$url['pass']);
+}
 return [
 
     /*
