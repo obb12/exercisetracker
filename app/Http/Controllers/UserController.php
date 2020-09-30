43805f8dbc84def5cb245339963bb4de580d25b9
Omar Benchbana <omar.bb@hotmail.fi>
2020-09-30 10:25:36 +0300

    first

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Models\Exercise;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+
+    public function create(Request $request)
+    {
+      $user = new User;
+      $user->username = $request->username;
+      $user->save();
+      return response()
+            ->json(['username' => $user->username, '_id' => $user->id]);
+    }
+    public function createexercise(Request $request)
+    {
+      $excercise = new Exercise;
+      $excercise->user_id = $request->userId;
+      $excercise->description = $request->description;
+      $excercise->duration = $request->duration;
+      $excercise->date   = $request->date;
+      $excercise->save();
+      return $excercise;
+
+    }
+    public function showlog(Request $request)
+    {
+      $excercise = Exercise::where('user_id', $request->userId)->get();
+      return $excercise;
+    }
+  
+}
---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+  return response(view('index'));
+});
+Route::post('/api/exercise/new-user', [UserController::class, 'create']);
+Route::post('/api/exercise/add', [UserController::class, 'createexercise']);
+Route::get('/api/exercise/log', [UserController::class, 'showlog']);
